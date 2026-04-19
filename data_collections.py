98619dac7d2cff08f6c7e0a7e60bef3f2d7137dc
Endija <endijatarvane@gmail.com>
2026-04-19 15:47:24 +0300

    feat: add list slicing (first, last, step)

--- data_collections.py
+++ data_collections.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 6]
 numbers.append(6)
 numbers.pop()
 
@@ -14,3 +14,14 @@ print("Count:", count)
 
 average = total / count
 print("Average:", average)
+
+even_numbers = []
+for num in numbers:
+    if num % 2 == 0: # parbauda, vai skaitlis ir pāra skaitlis. Ja num % 2 ir vienāds ar 0, tas nozīmē, ka num ir pāra skaitlis.
+        even_numbers.append(num)
+print("Even numbers:", even_numbers)
+
+print("\n ---Slice ---")
+print("First three numbers:", numbers[0:3])
+print("Last three numbers:", numbers[-3:])
+print("Numbers from index 2 to 4:", numbers[2:5])
